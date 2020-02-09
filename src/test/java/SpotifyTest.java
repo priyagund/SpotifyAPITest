37171fbb946ac5c1966202be3404eee50b5d0c5a
priyagund <priyankagund18@gmail.com>
2020-02-09 20:46:21 +0530

    REFACTOR: changes in variable name

--- src/test/java/SpotifyTest.java
+++ src/test/java/SpotifyTest.java
@@ -14,11 +14,12 @@ import org.junit.Test;
 
 public class SpotifyTest {
 
-    String id;
+    String userId;
     String playListId;
     private String tokenvalue = "Bearer BQC7_EagbX8RAngsjTVPBdSiXAX0r-UfKM5zZhbvKC7Da0s_a-3-GTKxmR3uLJmKyjLyllQxb-yJNdszXx09RF28ctR9V4dtA4UJ1C2RGoGQJ07avY5jSB60Rvf0AqohvRf0eWlfkL3e41pX6S2w2VN8RATtbDarh-LKEg-Ci8k67UgFFHHvSiPTQkpNINxyci2Kte5xnXwZmwuUFq5X4RHIn2h-XW8UbVRdWSNFgP5MKpDwVa9HSGkzbQPw90TTbIAfb20UYyZjqPAXWMag3x_3GYHwsw";
     Response response;
 
+    //Test for get user Id
     @Test
     public void givenMethodForGetId() throws ParseException {
         Response response = RestAssured.given()
@@ -29,37 +30,38 @@ public class SpotifyTest {
                 .get("https://api.spotify.com/v1/me");
         ResponseBody body = response.getBody();
         JSONObject object = (JSONObject) new JSONParser().parse(body.prettyPrint());
-        id = (String) object.get("id");
-        System.out.println(id);
+        userId = (String) object.get("id");
+        System.out.println(userId);
         response.then().assertThat().statusCode(200);
 
         //for verifing user
-        System.out.println(id);
-        Response response2 = RestAssured.given()
+        System.out.println(userId);
+        Response responseForUser = RestAssured.given()
                 .accept("application/json")
                 .contentType("application/json")
                 .header("Authorization", tokenvalue)
                 .when()
-                .get("https://api.spotify.com/v1/users/" + id);
-        response2.prettyPrint();
-        response2.then().assertThat().statusCode(200);
+                .get("https://api.spotify.com/v1/users/" + userId);
+        responseForUser.prettyPrint();
+        responseForUser.then().assertThat().statusCode(200);
 
         //create playlist
-        Response response3 = RestAssured.given()
+        Response responseForCreatePL = RestAssured.given()
                 .accept("application/json")
                 .contentType("application/json")
                 .header("Authorization", tokenvalue)
                 .body("{\"name\": \"New add Playlist 1\",\"description\": \"New playlist description\",\"public\": false" +
                         "}")
                 .when()
-                .post("https://api.spotify.com/v1/users/" + id + "/playlists");
+                .post("https://api.spotify.com/v1/users/" + userId + "/playlists");
         response.prettyPrint();
         response.then().assertThat().statusCode(200);
     }
 
+    //Test for finding count of of playlist
     @Test
     public void givenMethodForGettingPlaylistCountAndUpdatePlayList() throws ParseException {
-        Response response2 = RestAssured.given()
+        Response responseForPlayListCount = RestAssured.given()
                 .header("Accept", "application/json")
                 .header("Content-Type", "application/json")
                 .header("Authorization", tokenvalue)
@@ -67,9 +69,9 @@ public class SpotifyTest {
                 .get("https://api.spotify.com/v1/me/playlists")
                 .then()
                 .extract().response();
-        int playlistCount = response2.path("total");
-        String playListId = response2.path("items[1].id");
-        response2.prettyPrint();
+        int playlistCount = responseForPlayListCount.path("total");
+        String playListId = responseForPlayListCount.path("items[1].id");
+        responseForPlayListCount.prettyPrint();
         System.out.println("Playlist count: " + playlistCount);
         System.out.println("palylist id: " + playListId);
 
@@ -86,5 +88,4 @@ public class SpotifyTest {
         responseOfPlaylist.then().assertThat().statusCode(200);
 
     }
-
 }
